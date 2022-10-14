c84b97ba4e1d8f05c6a42e6dc5d4dddd5bdb98c2
Edward Tanguay (windows) <edward@tanguay.info>
2022-10-14 15:49:36 +0200

    showing techbooks with style

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import './App.scss';
 import techbooks from './data/techbooks.json';
 
-console.log(techbooks)
+console.log(techbooks);
 
 function App() {
 	return (
 		<div className="App">
-      <h1>{techbooks.length} Tech Books</h1>
-      {techbooks.map(techbook => {
-        return (
-          <div>{techbook.title}</div>
-        )
-      })}
+			<h1>{techbooks.length} Tech Books</h1>
+			<div className="books">
+				{techbooks.map((techbook) => {
+					return (
+						<div className="book">
+							<div className="title">{techbook.title}</div>
+							<div className="description">{techbook.description}</div>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
